93488083cc9878e598f246141a35d5c09e926ef3
Anotida Shumba <shumbaanotidaibm@gmail.com>
2025-11-30 22:38:33 +0200

    implimented map into main file

--- main.lua
+++ main.lua
@@ -8,14 +8,7 @@ local tilemap = love.graphics.newImage("assets/tilemap.png")
 local map_position = {x = 0, y = 0}
 local map = Map.new("assets/example.json", tilemap, 32, map_position)
 
-function love.load()
-    
-end
-
-function love.update(dt)
-    
-end
 
 function love.draw()
-    
+    map:draw()
 end
