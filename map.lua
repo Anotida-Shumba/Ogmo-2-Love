b10f43911290a975be96b858e141f457098bb41c
Anotida Shumba <shumbaanotidaibm@gmail.com>
2025-12-03 08:59:48 +0200

    combined map and tile file

--- map.lua
+++ map.lua
@@ -1,10 +1,37 @@
+---@class Tile
+---@field position Vec2
+---@field draw fun(self:Tile)
+
+Tile = {}
+Tile.__index = Tile
+
+---@return Tile
+---@param position Vec2
+---@param tile_coords Vec2
+---@param tilesize any
+---@param tilemap any
+function Tile.new(position, tile_coords, tilesize, tilemap)
+    local o = {}
+    setmetatable(o, Tile)
+    o.position = position
+    o.tilesize = tilesize
+    o.tile_coords = tile_coords
+    o.tilemap = tilemap
+    o.quad = love.graphics.newQuad(o.tile_coords.x * o.tilesize , o.tile_coords.y * o.tilesize , o.tilesize , o.tilesize , o.tilemap)
+    return o
+end
+
+function Tile:draw()
+    love.graphics.draw(self.tilemap , self.quad , self.position.x, self.position.y)
+end
+
 ---@class Map
 ---@field draw fun()
 
 Map = {}
 Map.__index = Map
 
-require "tile"
+-- require "tile"
 local json = require "dkjson" --path to dkjson
 
 ---@return Map
